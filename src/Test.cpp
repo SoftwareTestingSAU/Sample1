bbedc27c60847f43afcf2384638ae5d1f4f66274
MFA <fatih@returnsoft.com.tr>
2023-01-07 15:03:53 +0300

    Yeni kisi 4 nesnesi eklendi.

--- src/Test.cpp
+++ src/Test.cpp
@@ -3,12 +3,15 @@
 int main(){
 	Kisi *k1 = new Kisi("Murat",25,178);
 	Kisi *k2 = new Kisi("Meltem",32,150);	
-	Kisi *k3 = new Kisi("Leyla",28,142);
+	Kisi *k3 = new Kisi("Leyla",28,142);	
+	Kisi *k4 = new Kisi("Ahmet",45,157);
 	cout<<*k1;
 	cout<<*k2;
 	cout<<*k3;
+	cout<<*k4;
 	delete k1;
 	delete k2;
 	delete k3;
+	delete k4;
 	return 0;
 }
