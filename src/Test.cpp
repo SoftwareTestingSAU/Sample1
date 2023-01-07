900cf1829c18425f013c43315f82ce1fbb4dd127
SoftwareTestingSAU <122154278+SoftwareTestingSAU@users.noreply.github.com>
2023-01-07 20:08:44 +0300

    k3 silindi

--- src/Test.cpp
+++ src/Test.cpp
@@ -3,12 +3,10 @@
 int main(){
 	Kisi *k1 = new Kisi("Murat",25,178);
 	Kisi *k2 = new Kisi("Meltem",32,150);	
-	Kisi *k3 = new Kisi("Leyla",28,142);
 	cout<<*k1;
 	cout<<*k2;
 	cout<<*k3;
 	delete k1;
 	delete k2;
-	delete k3;
 	return 0;
 }
