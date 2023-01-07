484c820cc8c4fecbf9237c77a4ec3188295d71a2
unknown <fatih@returnsoft.com.tr>
2023-01-07 14:24:56 +0300

    Olusturulan kisi nesne sayisi 1 azaldi

--- src/Test.cpp
+++ src/Test.cpp
@@ -3,12 +3,9 @@
 int main(){
 	Kisi *k1 = new Kisi("Ahmet",25,178);
 	Kisi *k2 = new Kisi("Meltem",32,150);
-	Kisi *k3 = new Kisi("Ayse",42,168);
 	cout<<*k1;
 	cout<<*k2;
-	cout<<*k3;
 	delete k1;
 	delete k2;
-	delete k3;
 	return 0;
 }
