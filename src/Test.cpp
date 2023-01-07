e3b1cc07a60e2f386fea3b6f9431a27ca57e9aab
MFA <fatih@returnsoft.com.tr>
2023-01-07 20:03:55 +0300

    Bir kisi nesnesi testten silindi

--- src/Test.cpp
+++ src/Test.cpp
@@ -3,14 +3,12 @@
 int main(){
 	Kisi *k1 = new Kisi("Murat",25,178);
 	Kisi *k2 = new Kisi("Meltem",32,150);	
-	Kisi *k3 = new Kisi("Leyla",28,142);	
-	Kisi *k4 = new Kisi("Ahmet",45,157);
+	Kisi *k3 = new Kisi("Leyla",28,142);
 	cout<<*k1;
 	cout<<*k2;
 	cout<<*k3;
 	delete k1;
 	delete k2;
 	delete k3;
-	delete k4;
 	return 0;
 }
